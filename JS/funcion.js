96a92a0956357f5a8caeafadc0e3c3d9a58c4829
joss347 <141203525+joss347@users.noreply.github.com>
2023-08-01 22:17:04 -0600

    initial commit

---
+++ JS/funcion.js
@@ -0,0 +1,15 @@
+this.logueado = false;
+$("Entrada").click(function()(
+    if($("#usuario").val() == "Admin" && $("#contraseña").val()=='54321'){
+        logueado = true;
+        if(logueado == true){
+            //swal.fire('Usuario correcto')
+            window.location
+        }
+    }
+    else(
+        swal.fire('Usuario Incorrecto')
+        
+    )
+)
+    )
